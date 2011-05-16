501ef684cffdf597f16f63eb16c11a72fc0f08bd
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-05-16 17:56:59 +0100

    First version.  Adding num2str function.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ lib/puppet/parser/functions/num2str.rb
@@ -0,0 +1,96 @@
+#
+# num2bool.rb
+#
+# Copyright 2011 Puppet Labs Inc.
+# Copyright 2011 Krzysztof Wilczynski
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Puppet::Parser::Functions
+  newfunction(:num2str, :type => :rvalue, :doc => <<-EOS
+Returns the string representation of any numeric data type after conversion.
+
+Prototype:
+
+    num2str(x)
+
+Where x is a numeric value.
+
+For example:
+
+  Given the following statements:
+
+    $a = 1
+    $b = 1.0
+    $c = -1
+    $d = -1.0
+
+    notice num2str($a)
+    notice num2str($b)
+    notice num2str($c)
+    notice num2str($d)
+
+  The result will be as follows:
+
+    notice: Scope(Class[main]): 1
+    notice: Scope(Class[main]): 1.0
+    notice: Scope(Class[main]): -1
+    notice: Scope(Class[main]): -1.0
+    EOS
+  ) do |arguments|
+
+    #
+    # We put arguments that are strings back into the array.  This is
+    # to ensure that whenever we call this function from within the
+    # Puppet manifest or alternatively form a template it will always
+    # do the right thing ...
+    #
+    if arguments.is_a?(String)
+      arguments = [ arguments ]
+    end
+
+    if arguments.size < 1
+      raise(Puppet::ParseError, "num2str(): Wrong number of arguments " +
+        "given (#{arguments.size} for 1)")
+    end
+
+    numeric = arguments[0]
+    klass   = numeric.class
+
+    # This should cover all the generic numeric types present in Puppet ...
+    unless klass.ancestors.include?(Numeric) or numeric.is_a?(String)
+      raise(Puppet::ParseError, 'num2str(): Requires ' +
+        'a numeric value to work with')
+    end
+
+    # Numbers in Puppet are often string-encoded which is troublesome ...
+    if numeric.is_a?(String)
+      if numeric.match(/^-?(?:\d+)(?:\.\d+){1}$/) # Floating point
+        numeric = numeric.to_f
+      elsif numeric.match(/^-?\d+$/)              # Integer
+        numeric = numeric.to_i
+      else
+        raise(Puppet::ParseError, 'num2str(): Requires ' +
+          'either integer or float to work with')
+      end
+    end
+
+    # We have a numeric value to handle ...
+    result = numeric.to_s
+
+    return result
+  end
+end
+
+# vim: set ts=2 sw=2 et :
