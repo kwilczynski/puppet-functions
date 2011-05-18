f0ed1e7a35f3056b7aadd5fc456cdc120bba07a0
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-05-18 18:32:47 +0100

    First version.  Adding str2bool function.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ lib/puppet/parser/functions/str2bool.rb
@@ -0,0 +1,103 @@
+#
+# str2bool.rb
+#
+# Copyright 2011 Krzysztof Wilczynski
+# Copyright 2011 Puppet Labs Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Puppet::Parser::Functions
+  newfunction(:str2bool, :type => :rvalue, :doc => <<-EOS
+Returns a boolean value after conversion from boolean-alike textual representation.
+
+Prototype:
+
+    str2bool(s)
+
+Where s is a string given in one of the following boolean-alike forms:
+
+  When given as either '1', 't', 'y', 'true' or 'yes' it will evaluate
+  as true whereas when given as either '0', 'f', 'n', 'false' or 'no'
+  then it will evaluate as false.
+
+For example:
+
+  Given the following statements:
+
+    $a = 'yes'
+    $b = 'no'
+    $c = 't'
+    $d = 'f'
+    $e = '1'
+    $f = '0'
+
+    notice str2bool($a)
+    notice str2bool($b)
+    notice str2bool($c)
+    notice str2bool($d)
+    notice str2bool($e)
+    notice str2bool($f)
+
+  The result will be as follows:
+
+    notice: Scope(Class[main]): true
+    notice: Scope(Class[main]): false
+    notice: Scope(Class[main]): true
+    notice: Scope(Class[main]): false
+    notice: Scope(Class[main]): true
+    notice: Scope(Class[main]): false
+    EOS
+  ) do |arguments|
+
+    #
+    # We put arguments that are strings back into the array.  This is
+    # to ensure that whenever we call this function from within the
+    # Puppet manifest or alternatively form a template it will always
+    # do the right thing ...
+    #
+    if arguments.is_a?(String)
+      arguments = [ arguments ]
+    end
+
+    if arguments.size < 1
+      raise(Puppet::ParseError, "str2bool(): Wrong number of arguments " +
+        "given (#{arguments.size} for 1)")
+    end
+
+    string = arguments[0]
+
+    unless string.is_a?(String)
+      raise(Puppet::ParseError, 'str2bool(): Requires ' +
+        'a string to work with')
+    end
+
+    # We consider all the boolean-alike values too ...
+    result = case string
+      #
+      # This is how undef looks like in Puppet ...
+      # We yield false in this case.
+      #
+      when /^$/, ''              then false # Empty string will be false ...
+      when /^(1|t|y|true|yes)$/  then true
+      when /^(0|f|n|false|no)$/  then false
+      when /^(undef|undefined)$/ then false # This is not likely to happen ...
+      else
+        raise(Puppet::ParseError, 'str2bool(): Unknown type of boolean given')
+    end
+
+    return result
+  end
+end
+
+# vim: set ts=2 sw=2 et :
