a2d5f0354b048262a1614a571d47c9b07090fa39
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-08-21 22:42:48 +0100

    Add new functions stub.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ lib/puppet/parser/functions/hex2num.rb
@@ -0,0 +1,48 @@
+#
+# hex2num.rb
+#
+# Copyright 2011 Puppet Labs Inc.
+# Copyright 2011 Krzysztof Wilczynski
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Puppet::Parser::Functions
+  newfunction(:hex2num, :type => :rvalue, :doc => <<-EOS
+Returns
+
+Prototype:
+
+
+
+Where
+
+For example:
+
+  Given the following statements:
+
+  The result will be as follows:
+
+    EOS
+  ) do |*arguments|
+    #
+    # This is to ensure that whenever we call this function from within
+    # the Puppet manifest or alternatively form a template it will always
+    # do the right thing ...
+    #
+    arguments = arguments.shift if arguments.first.is_a?(Array)
+
+  end
+end
+
+# vim: set ts=2 sw=2 et :
