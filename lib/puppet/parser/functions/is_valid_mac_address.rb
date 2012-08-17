f537ce211e5e4c399203a1810849e637d99aaacb
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2012-08-17 01:27:45 +0100

    Add implementation to the function is_valid_mac_address.
    Adding implementation of the said function for use within Puppet DSL,
    plus relevant documentation and examples.
    
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- lib/puppet/parser/functions/is_valid_mac_address.rb
+++ lib/puppet/parser/functions/is_valid_mac_address.rb
@@ -18,20 +18,42 @@
 
 module Puppet::Parser::Functions
   newfunction(:is_valid_mac_address, :type => :rvalue, :doc => <<-EOS
-Returns
+Returns true if given string value is a valid MAC address and false otherwise.
 
 Prototype:
 
-    is_valid_mac_address()
+    is_valid_mac_address(s)
 
-Where
+Where s is a string value that resembles IEEE 802 standard (including the
+Microsoft variant), Cisco style or Sun Microsystems MAC address.
+
 
 For example:
 
   Given the following statements:
 
+    $a = '4E:55:35:AD:17:DF'
+    $b = '92:1H:8E:8C:89:A5'
+    $c = 'A9-79-FA-9D-79-A8'
+    $d = '36A0.8B54.9FD7'
+    $e = 'C:0:F:F:E:42'
+    $f = '0C:C:B:FF:35:5C'
+
+    notice is_valid_mac_address($a)
+    notice is_valid_mac_address($b)
+    notice is_valid_mac_address($c)
+    notice is_valid_mac_address($d)
+    notice is_valid_mac_address($e)
+    notice is_valid_mac_address($f)
+
   The result will be as follows:
 
+    notice: Scope(Class[main]): true
+    notice: Scope(Class[main]): false
+    notice: Scope(Class[main]): true
+    notice: Scope(Class[main]): true
+    notice: Scope(Class[main]): true
+    notice: Scope(Class[main]): false
     EOS
   ) do |*arguments|
     #
@@ -41,6 +63,43 @@ For example:
     #
     arguments = arguments.shift if arguments.first.is_a?(Array)
 
+    raise Puppet::ParseError, "is_valid_mac_address(): Wrong number of arguments " +
+      "given (#{arguments.size} for 1)" if arguments.size < 1
+
+    mac_address = arguments.shift
+
+    raise Puppet::ParseError, 'is_valid_mac_address(): Requires a string type ' +
+      'to work with' unless mac_address.is_a?(String)
+
+    result = case mac_address
+      #
+      # The IEEE 802 standard MAC address which is a six groups of two
+      # hexadecimal digits and has ":" (single colon) as delimiter.
+      #
+      # Microsoft variant has the same overall format but uses "-" (single
+      # hyphen) as delimiter instead.
+      #
+      # Note that both version have hexadecimal digital zero-padded ...
+      #
+      when /^([0-9a-fA-F]{2}[:\-]){5}[0-9a-fA-F]{2}$/ then true
+      #
+      # Cisco uses MAC address which is a three groups of four hexadecimal
+      # digital as has "." (single dot) as delimiter ...
+      #
+      when /^([0-9a-fA-F]{4}\.){2}[0-9a-fA-F]{4}$/ then true
+      #
+      # Sun Microsystems standard MAC address is very similar to the
+      # IEEE 802 standard with the difference that hexadecimal digits
+      # are not zero-padded at all and should be lower-case ...
+      #
+      when /^((0|[1-9]{1,2}|[a-fA-F]{1,2}):){5}
+              (0|[1-9]{1,2}|[a-fA-F]{1,2}){2}$/x then true
+      # Otherwise no match ...
+      else
+        false
+    end
+
+    result
   end
 end
 
